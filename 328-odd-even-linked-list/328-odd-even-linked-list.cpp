6568e1027bf1196605dfb64dcbfc7d84ef22a429
Rajath <74639880+Rajath-Acharya@users.noreply.github.com>
2022-07-08 20:26:00 +0530

    Time: 21 ms (39.04%), Space: 10.5 MB (75.26%) - LeetHub

---
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL ||
+           head -> next == NULL ||
+           head -> next -> next == NULL)
+        {
+            return head;
+        }
+        
+        ListNode* odd = head;
+        ListNode* even = head -> next;
+        ListNode* oddHead = odd;
+        ListNode* evenHead = even;
+        int cnt = 3;
+        ListNode* start = even -> next;
+        while(start != NULL) {
+            if(cnt % 2 == 0) {
+                even -> next = start;
+                even = even -> next;
+            } else {
+                odd -> next = start;
+                odd = odd -> next;
+            }
+            cnt++;
+            start = start -> next;
+        }
+        odd -> next = NULL;
+        even -> next = NULL;
+        odd -> next = evenHead;
+        return oddHead;
+    }
+};
